d396a97cf8fc74d5e1bfb44f64d476861682bb39
Pasha <p.s.riazanov@kalashnikovconcern.ru>
2019-11-27 23:38:33 +0300

    init

--- app/src/main/java/com/example/testnewdialog/CoordinateDialog.java
+++ app/src/main/java/com/example/testnewdialog/CoordinateDialog.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 public class CoordinateDialog extends DialogFragment {
     @Nullable
